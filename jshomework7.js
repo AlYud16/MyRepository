3a23a7dc0f06864a0ea85a0cead21f8f9956c7d2
Alina Yudina <alyudina50@gmail.com>
2025-11-22 13:46:54 +0200

    Added regular expressions

---
+++ jshomework7.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let words = ['Wonderful', 'Happiness', 'Time', 'Task', 'Joyful', 'Apple'];
+
+let res = /\b[^Aa]{6,}\b/;
+
+let result = [];
+
+for (let i = 0; i < words.length; i++) {
+    if (res.test(words[i])) {
+        result[result.length] = words[i];
+    }
+    
+}
+
+console.log(result);
